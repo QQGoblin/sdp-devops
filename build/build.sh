691a11bd9e476d876c9b6871927b26fc6ff7f60a
LinQing2017 <m15651620957@163.com>
2020-11-14 18:33:33 +0800

    添加Dockerfile，以及chart文件

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,8 @@ VERSION=v0.4.0
 IMAGE=wxext-registry.101.com/sdp/sdp-devops
 
 cd ../
-go build -o bin/sdp-cleaner cmd/sdp-cleaner/sdp-cleaner.go
-go build -o bin/sdp-exporter cmd/sdp-exporter/sdp-exporter.go
+go build -o build/bin/sdp-cleaner cmd/sdp-cleaner/sdp-cleaner.go
+go build -o build/bin/sdp-exporter cmd/sdp-exporter/sdp-exporter.go
 
 cd -
 
