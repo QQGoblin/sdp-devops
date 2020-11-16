fe76f5f45af72193db7f2483c82900a4719969d2
LinQing <m15651620957@163.com>
2020-11-16 17:31:41 +0800

    fix：使用内网ip发现服务

--- pkg/logger/sdp_logger.go
+++ pkg/logger/sdp_logger.go
@@ -9,7 +9,7 @@ var Log *logrus.Logger
 
 func InitLogger() {
 	logrus.SetOutput(os.Stdout)
-	logrus.SetFormatter(&logrus.JSONFormatter{})
+	//logrus.SetFormatter(&logrus.JSONFormatter{})
 	logrus.SetLevel(logrus.InfoLevel)
 	logrus.WithFields(logrus.Fields{
 		"sdp-devops": "Kubernetes",
