72d4d41fdeb8ba9790dd7142e6361f062f0e2f69
LinQing2017 <m15651620957@163.com>
2020-11-21 19:39:47 +0800

    featuer: 添加Docker工具

--- pkg/sdpctl/cmd/shell_docker_net.go
+++ pkg/sdpctl/cmd/shell_docker_net.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	dockertools "sdp-devops/pkg/util/docker"
 	systools "sdp-devops/pkg/util/sys"
@@ -11,13 +12,17 @@ import (
 
 func RunShellDockerNet(cmd *cobra.Command, args []string) {
 
+	if len(args) < 1 {
+		logrus.Error("请输入要执行的命令")
+	}
+
 	cmdStr := strings.Join(args, " ")
 	cli := dockertools.DockerClient("")
 	podBriefInfoList := dockertools.GetPodInfo(cli)
 
 	for i, podBriefInfo := range podBriefInfoList {
 		fmt.Println("============================= No.", i, "POD:", podBriefInfo.Name, "=============================")
-		if outStr, errStr, err := systools.CmdOutErr("/usr/bin/nsenter", "-t", strconv.Itoa(podBriefInfo.PID), "-n", cmdStr); err != nil {
+		if outStr, errStr, err := systools.CmdOutErr("/usr/bin/nsenter", "-t", strconv.Itoa(podBriefInfo.PID), "-n", "/bin/sh", "-c", cmdStr); err != nil {
 			fmt.Println(errStr)
 			fmt.Println(err.Error())
 		} else {
